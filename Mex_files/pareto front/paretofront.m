0c2c068efd23b984a87cc490472ccfc266d4fb13
Eric Bradford <32679792+Eric-Bradford@users.noreply.github.com>
2018-10-29 14:30:53 +0100

    Add files via upload

---
+++ Mex_files/pareto front/paretofront.m
@@ -0,0 +1,50 @@
+% Copyright (c) 2006, Eduard Polityko
+% All rights reserved.
+% 
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are
+% met:
+% 
+%     * Redistributions of source code must retain the above copyright
+%       notice, this list of conditions and the following disclaimer.
+%     * Redistributions in binary form must reproduce the above copyright
+%       notice, this list of conditions and the following disclaimer in
+%       the documentation and/or other materials provided with the distribution
+% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGE.
+%
+function b = prtp(B)
+A=[]; varargout{1}=[];
+sz1=size(B,1);
+jj=0; kk(sz1)=0;
+c(sz1,size(B,2))=0;
+bb=c;
+for k=1:sz1
+  j=0;
+  ak=B(k,:);
+  for i=1:sz1
+    if i~=k
+      j=j+1;
+      bb(j,:)=ak-B(i,:);
+    end
+  end
+  if any(bb(1:j,:)'<0)
+    jj=jj+1;
+    c(jj,:)=ak;
+    kk(jj)=k;
+  end
+end
+  A            = c(1:jj,:);
+  varargout{1} = kk(1:jj);
+  b = zeros(sz1,1); b(kk(1:jj)) = 1;
+end
