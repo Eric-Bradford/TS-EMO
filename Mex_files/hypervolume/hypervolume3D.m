7ab2759fb7a2b92e5e281000735a93e048d02f12
Eric Bradford <32679792+Eric-Bradford@users.noreply.github.com>
2018-10-31 22:20:15 +0100

    Update hypervolume3D.m

--- Mex_files/hypervolume/hypervolume3D.m
+++ Mex_files/hypervolume/hypervolume3D.m
@@ -122,3 +122,32 @@ function L = nd_filter(L, spawn_vector, ub)
 	end
 	L = [spawn_vector(:,find(do_assign == 1)), L];
 end
+
+function d = weakly_dominates(fA, fB)
+% [d] = weakly_dominates(fA, fB)
+%
+% Compares two solutions A and B given their objective function
+% values fA and fB. Returns whether A weakly dominates B.
+%
+% Input:
+% - fA					- The objective function values of solution A
+% - fB					- The objective function values of solution B
+%
+% Output:
+% - d					- d is 1 if fA dominates fB, otherwise d is 0 
+%
+% Author: Johannes W. Kruisselbrink
+% Last modified: March 17, 2011
+
+	% Elegant, but not very efficient
+	%d = (all(fA <= fB) && any(fA < fB));
+
+	% Not so elegant, but more efficient
+	d = true;
+	for i = 1:length(fA)
+		if (fA(i) > fB(i))
+			d = false;
+			return
+		end
+	end
+end
