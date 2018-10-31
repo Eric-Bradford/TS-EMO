ebbe4f4819ddb44b5ab2010918f2bcce484f9a6d
Eric Bradford <32679792+Eric-Bradford@users.noreply.github.com>
2018-10-31 21:57:33 +0100

    Update startup.m

--- startup.m
+++ startup.m
@@ -1,8 +1,7 @@
-%Startup Script for TS-EMO Algorithm
+%Startup script for TS-EMO Algorithm
 
-%Add function folders to path
-folders = {'Test_functions', 'NGPM_v1.4', 'Mex_files/hypervolume', 'Mex_files/invchol', 'Mex_files/pareto front' 'Direct'};
-for i = 1:length(folders)
-    new_path = strcat(pwd, '/', folders{i}, '/');
-    addpath(new_path);
-end
+disp ('executing TS-EMO startup script...')
+mydir = fileparts (mfilename ('fullpath')); % where am I located
+addpath (mydir)
+folders = {'Test_functions', 'NGPM_v1.4', 'Mex_files/hypervolume', 'Mex_files/invchol', 'Mex_files/pareto front', 'Direct'}; 
+for d = folders, addpath (fullfile (mydir, d{1})), end
